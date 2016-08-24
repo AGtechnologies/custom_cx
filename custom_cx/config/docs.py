7fb1802cd84bb4160a79fa17cb80c0547b644eca
justinlu-agtechsg <info@agtech.com.sg>
2016-08-25 02:05:10 +0800

    Update docs.py

--- custom_cx/config/docs.py
+++ custom_cx/config/docs.py
@@ -8,4 +8,4 @@ Configuration for docs
 # sub_heading = "Yes, you got that right the first time, everything"
 
 def get_context(context):
-	context.brand_html = "Custom Cx"
+	context.brand_html = "CX3 Asia Pte Ltd"
