e8eadc6d8d0d13de7674298d2e1e0797c5a3e7f8
justinlu-agtechsg <info@agtech.com.sg>
2016-08-25 01:53:59 +0800

    Update desktop.py

--- custom_cx/config/desktop.py
+++ custom_cx/config/desktop.py
@@ -5,10 +5,10 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"module_name": "Custom Cx",
+			"module_name": "CX3",
 			"color": "grey",
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
-			"label": _("Custom Cx")
+			"label": _("CX3")
 		}
 	]
